c14205f438a1a33d0f7e92be1385df90bc998f27
QuietMoth1 <quietmoth1@gmail.com>
2022-12-02 15:16:49 +0400

    Fix Bugs

--- passadder.go
+++ passadder.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
 	"bufio"
+	"fmt"
+	"flag"
+	"os"
     "github.com/fatih/color"
 )
 
 // Function to check if a file exists in a dictionary
-func password_check(filepath string, user_password string) { 
-	file, err := os.Open(filepath)
+func password_check(wordlist string, password_to_add string) { 
+	file, err := os.Open(wordlist)
 	if err != nil {
 		fmt.Errorf("%v", err)
 	}
@@ -18,17 +18,14 @@ func password_check(filepath string, user_password string) {
 
 	// Splitting the file into lines
 	scanner := bufio.NewScanner(file)
-
-	line := 1 // We read the lines where the password is located
+	
 	result := 0 // Results counter
 	
 	for scanner.Scan() {
 		// If the password is contained in a dictionary, then we add our counter to 1
-		if strings.Contains(scanner.Text(), user_password) { 
+		if scanner.Text() == password_to_add { 
 			result++
-		} 
-
-		line++
+		}
 	} 
 
 
@@ -38,41 +35,40 @@ func password_check(filepath string, user_password string) {
 
 	// If the result does not exceed 1 (it means that the password is not written to the dictionary), then we pass it to the add function
 	if result < 1 {
-		add_passwords(result, user_password, filepath)
-	} else {
-        user_password = color.RedString(user_password)
-    	fmt.Println("Password -", user_password, "already exists -", result, "time!")    // Проверка на наличие пароля 
-	}
-	
+		add_passwords(result, password_to_add, wordlist)
+	} 
 }
 
 
 // Function to add a password to a file
-func add_passwords(result int, user_password string, filepath string) {
-    user_password = color.RedString(user_password)
-	fmt.Println("\n\tPassword - ", user_password, "was added!") 
+func add_passwords(result int, password_to_add string, wordlist string) {
 
-	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY, 0600)
+	f, err := os.OpenFile(wordlist, os.O_APPEND|os.O_WRONLY, 0600)
     if err != nil {
         panic(err)
     }
     defer f.Close()
 
-    _, err = f.WriteString("\n" + user_password)
+    _, err = f.WriteString(password_to_add + "\n")
 	if err != nil {
 		panic(err)
 	}
+
+	password_to_add = color.RedString(password_to_add)
+	fmt.Println("\n\tPassword -", password_to_add, "was added!") 
 }
-
-
 
 func main() {
 	// TO-DO Add line-by-line reading of passwords from another file and passing them to the goroutine channel
-	fmt.Print("Enter password: ")
-	var user_password string
-	fmt.Scanf("%s", &user_password)
+	password_to_add := flag.String("password", "", "your file with passwords")
+	wordlist := flag.String("wordlist", "", "your wordlist")
+	flag.Parse()
 
-	filepath := "/root/passwords.txt" // use your dictionary file to add passwords to it  
-
-	password_check(filepath, user_password)
+	if len(*password_to_add) == 0 && len(*wordlist) == 0 {
+		fmt.Println("Usage: passadder -password password123 -wordlist wordlist.txt")
+		flag.PrintDefaults()
+		os.Exit(0)
+	} else {
+		password_check(*wordlist, *password_to_add)
+	}
 }
